197c04067e9cf1c13c18cabaaa2c5d81ec8dc7f0
paru <parvati.madyal@codecraft.co.in>
2026-02-11 15:36:43 +0530

    Documentation

---
+++ square.ts
@@ -0,0 +1,28 @@
+/**
+ * hii typo
+ * @param a
+ * @param b 
+ * @returns 
+ */
+function add(a:number,b:number): number{
+    return a+b;
+}
+
+function substract(a:number,b:number): number{
+    return a-b;
+}
+
+function multiply(a:number,b:number): number{
+    return a-b;}
+
+function sqaure(a:number): number{
+    return a*a ;}
+
+function cube(a:number): number{
+    return a*a ;}
+
+function four(a:number): number{
+    return a*a*a*a ;}
+
+function five(a:number): number{
+    return a*a*a*a*a ;}
